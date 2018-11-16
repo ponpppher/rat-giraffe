7b9dcd9f54f839d9b3ad2476ce17397b9f79fa41
ponpppher <genmaier@gmail.com>
2018-11-16 16:40:07 +0900

    一覧ページ整形

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,7 +3,7 @@ class Feed < ApplicationRecord
   validates:rent, presence:true
   validates:address, presence:true
   validates:age, presence:true
-  validates:remarks, presence:true
+  validates:remarks, presence:true, length:{maximum:120}
 
   has_many :nearest_stations
   accepts_nested_attributes_for :nearest_stations
